ca5d0d34fe98ebbdf4015cee547c85db5c7dff47
Tarık Yazıcı <tarik_yazici_1905@hotmail.com>
2021-10-07 18:08:48 +0300

    Final commit

---
+++ android/app/src/main/kotlin/tarik/com/xylophone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package tarik.com.xylophone_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(soundNumber) {
+    final player = AudioCache();
+    player.play('assets_note$soundNumber.wav');
+  }
+
+  Widget buildKey({required Color color, required int number}) {
+    return Expanded(
+      child: TextButton(
+        child: Text(''),
+        style: TextButton.styleFrom(backgroundColor: color),
+        onPressed: () {
+          playSound(number);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, number: 1),
+              buildKey(color: Colors.orange, number: 2),
+              buildKey(color: Colors.yellow, number: 3),
+              buildKey(color: Colors.green, number: 4),
+              buildKey(color: Colors.teal, number: 5),
+              buildKey(color: Colors.blue, number: 6),
+              buildKey(color: Colors.purple, number: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
